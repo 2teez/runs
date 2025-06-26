8b428679aca164a53725648b1084f2f66eb78a5a
timothy adigun <adigun.timothy@gmail.com>
2025-06-26 12:25:14 +0100

    bin file for checking how the library would work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#![allow(dead_code, unused)]
+
+use runs::runs::gets_file_name;
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let filename = env::args().skip(1).next().expect("No file specified.");
+    let filename = gets_file_name(&filename);
+    println!("{:?}", filename);
 }
