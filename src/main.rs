adbd9a4b0764d3d57f0020a02fa873ccc9ab34ae
timothy adigun <adigun.timothy@gmail.com>
2025-06-30 10:36:17 +0100

    moved main.rs from src to bin

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-#![allow(dead_code, unused)]
-
-use runs::runs::{gets_file_name, read_file};
-use std::env;
-fn main() {
-    let filename = env::args().skip(1).next().expect("No file specified.");
-    let filename = gets_file_name(&filename).expect("can't read file");
-    println!("{:?}", read_file(&filename[..]));
-}
